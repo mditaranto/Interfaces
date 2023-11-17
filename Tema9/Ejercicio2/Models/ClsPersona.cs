70f6313b1e985c86c59ce411c2fd331afe1420f3
mditaranto <mditaranto@iesnervion.es>
2023-11-17 11:57:48 +0100

    Actividades

---
+++ Tema9/Ejercicio2/Models/ClsPersona.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    internal class ClsPersona : INotifyPropertyChanged
+    {
+
+        #region atributos
+        private int id;
+        private string nombre;
+        private string apellido;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region constructores
+        public ClsPersona()
+        {
+            id = 0;
+            nombre = "Matias";
+            apellido = "Ditaranto";
+        }
+
+        public ClsPersona(string nombre, string apellido, int idDepartamento, int id)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+
+        }
+        #endregion
+
+        #region propiedades
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public String Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String NombreCompleto
+        {
+            get { return $"{nombre} {apellido}"; }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
+        #endregion
+    }
+}
---
+++ Tema9/Tema9/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tema9
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
