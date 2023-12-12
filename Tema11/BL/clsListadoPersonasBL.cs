108dc902b2adf010e89db35a024fd65d7cc7e03e
mditaranto <mditaranto@iesnervion.es>
2023-12-12 14:37:12 +0100

    API

---
+++ Tema11/BL/clsListadoPersonasBL.cs
@@ -0,0 +1,23 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class clsListadoPersonasBL
+    {
+        /// <summary>
+        /// Funcion que llama a la DAL para obtener un listado de personas, aplicando las reglas de negocio que se establezcan
+        /// </summary>
+        /// <returns></returns>
+        public async static Task<List<ClsPersona>> listadoPersonasBL()
+        {
+            return await DAL.clsListadoPersonas.listadoPersonas();
+        }
+    }
+}
+ 
---
+++ Tema11/Ej1/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using BL;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ej1.ViewModel
+{
+    public class MainViewModel
+    {
+        private List<ClsPersona> _listadoPersonas;
+        private ObservableCollection<ClsPersona> listadoPersonas;
+
+        public ObservableCollection<ClsPersona> ListadoPersonas
+        {
+            get
+            {
+                return _listadoPersonas;
+            }
+        }
+
+        public MainViewModel()
+        {
+            cargarPersonas();
+            
+        }
+
+        private async Task cargarPersonas()
+        {
+            _listadoPersonas = await clsListadoPersonasBL.listadoPersonasBL();
+            listadoPersonas = new ObservableCollection<ClsPersona>(_listadoPersonas);
+        }
+    
+    }
+}
