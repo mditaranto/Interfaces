a9b970cb2e9fbfdc030c19d9df3841a8d249b5dc
mditaranto <mditaranto@iesnervion.es>
2024-02-01 11:49:29 +0100

    asd

---
+++ Kiriki/Kiriki/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Kiriki
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Kiriki/Kiriki/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Kiriki
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
