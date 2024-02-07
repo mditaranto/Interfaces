1cd55390da2603d3f9318b20154f1020b7f6071c
mditaranto <mditaranto@iesnervion.es>
2024-02-07 09:42:50 +0100

    asd

---
+++ Kiriki/KirikiServer/Hubs/KirikiHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace KirikiServer.Hubs
+{
+    public class KirikiHub : Hub
+    {
+        public async Task MostrarDado(String dado1, String dado2)
+        {
+            await Clients.All.SendAsync("MostrarDado", dado1, dado2);
+        }
+    }
+}
